df74dd77783676ea6ca5b607bf8fbb7dccd30ebb
hangmanpingou <xxhangmanxx@free.fr>
2024-10-05 19:42:53 +0200

    Interface graphique faite mais moche

---
+++ testeur_json.py
@@ -0,0 +1,41 @@
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QPushButton
+from PySide6 import QtCore, QtGui, QtWidgets
+
+class testeur_json(QMainWindow): 
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Testeur de fichier .JSON") # Titre de la fenêtre
+        self.setFixedSize(600, 250) # Taille bloqué de la fenêtre
+        self.zone_centrale = QWidget(self) # Définition de ma zone où vont apparaitre mes widgets
+        self.setCentralWidget(self.zone_centrale)
+        self.zone_centrale.setStyleSheet("background: #E3F9F1")
+        
+        self.presentation = QLabel("Le ou les fichiers à tester doivent se trouver dans le répertoire 'test'.", self.zone_centrale) # Le label du haut
+        self.presentation.setGeometry(25, 20, 550, 40)
+        self.presentation.setStyleSheet("font-size: 17px ; background: #E1F1D5 ; font-style: italic ; font-weight: bold")
+        self.presentation.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+
+        self.fichier_teste = QtWidgets.QComboBox(self.zone_centrale) # Le menu déroullant
+        self.fichier_teste.setGeometry(25, 80, 300, 60) # Coordonnées et taille
+        self.fichier_teste.setStyleSheet("background : #FFFF59") # Couleur de fond
+        self.fichier_teste.setEditable(True) # Permet d'exécuter setCurrentText, le setAlignement
+        self.fichier_teste.setCurrentText("Choisir le fichier : ") # Ce qui apparait de base dans le comboBox
+        self.fichier_teste.lineEdit().setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.fichier_teste.setStyleSheet("font-size: 17px ; font-weight: bold")
+
+        self.bouton = QPushButton("TESTER", self.zone_centrale)
+        self.bouton.setGeometry(387.5, 90, 150, 40)
+        self.bouton.setStyleSheet("background-color: blue ; border: 2px solid black ; border-radius: 75px 20px; font-size: 17px ; font-weight: bold")
+
+        self.resultat = QLabel("Le résultat sera afficher ici.", self.zone_centrale)
+        self.resultat.setGeometry(25, 165, 550, 60)
+        self.resultat.setStyleSheet("background: #E35CF1")
+        self.resultat.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.resultat.setStyleSheet("font-size: 17px ; background: #E1F1D5 ; font-style: italic ; font-weight: bold")
+
+
+app = QApplication()
+fenetre = testeur_json()
+fenetre.show()
+app.exec()
