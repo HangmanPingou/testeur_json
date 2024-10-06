cfd61e523cb49e547c0c5e41b1ec737980dc939b
hangmanpingou <xxhangmanxx@free.fr>
2024-10-06 15:27:52 +0200

    Mise en place du TEST et connection au bouton

--- testeur_json.py
+++ testeur_json.py
@@ -1,6 +1,7 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QPushButton, QComboBox
 from PySide6 import QtCore
 from pathlib import Path
+import json
 
 class testeur_json(QMainWindow): 
     def __init__(self):
@@ -20,25 +21,40 @@ class testeur_json(QMainWindow):
         self.liste_fichiers = [] # Création de la liste utilisé dans le ComboBox
         self.repertoire_test = Path.cwd() / "test" # Création de la variable Path qui contient l'adresse relative du script
         for i in self.repertoire_test.iterdir(): # Je créé un itérable et je boucle pour incrémenter ma liste
-            self.liste_fichiers.append(i.name)
+            self.liste_fichiers.append(i.name) # Le .name permet de n'afficher uniquement le fichier et son extension
 
         self.fichier_teste = QComboBox(self.zone_centrale) # Le menu déroullant
         self.fichier_teste.setGeometry(25, 80, 300, 60) # Coordonnées et taille
         self.fichier_teste.setEditable(True) # Permet d'exécuter setCurrentText, le setAlignement sur le ComboBox
         self.fichier_teste.lineEdit().setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
         self.fichier_teste.setStyleSheet("font-size: 17px ; font-weight: bold")
-        self.fichier_teste.addItems(self.liste_fichiers) # Ajoute dans le ComboBox tout se qui se trouve dans ma liste
+        self.fichier_teste.addItems(self.liste_fichiers) # Ajoute dans le ComboBox tout se qui se trouve dans ma
         self.fichier_teste.setCurrentText("Choisir le fichier : ") # Ce qui apparait de base dans le comboBox
 
         self.bouton = QPushButton("TESTER", self.zone_centrale) # Le bouton
         self.bouton.setGeometry(387.5, 90, 150, 40)
         self.bouton.setStyleSheet("background-color: blue ; border: 2px solid black ; border-radius: 75px 20px; font-size: 17px ; font-weight: bold")
+        self.bouton.clicked.connect(self.test)
 
         self.resultat = QLabel("Le résultat sera afficher ici.", self.zone_centrale) # Le label "RESULTAT"
         self.resultat.setGeometry(25, 165, 550, 60)
         self.resultat.setStyleSheet("background: #E35CF1")
         self.resultat.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
         self.resultat.setStyleSheet("font-size: 17px ; background: #E1F1D5 ; font-style: italic ; font-weight: bold")
+
+    def test(self):
+        fichier = self.fichier_teste.currentText()
+        try:
+            with open(f"test/{fichier}", "r") as f: # J'ouvre mon fichier et créé une variable (dictionnaire), puis j'ajoute un nouveau sous-dictionnaire
+                essai = json.load(f)
+                f.close()
+            self.resultat.setStyleSheet("font-size: 17px ; color: green ; font-style: italic ; font-weight: bold")
+            self.resultat.setText("Le fichier est valide")
+        except:
+            self.resultat.setStyleSheet("font-size: 17px ; color: red ; font-style: italic ; font-weight: bold")
+            self.resultat.setText("Le fichier n'est pas valide")
+
+
 
 
 
