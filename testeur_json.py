f4d21dcaa8fd04c769fc0d35d57ca89ed6902759
hangmanpingou <xxhangmanxx@free.fr>
2024-10-06 16:59:47 +0200

    Améliorations visuelles de l'interface

--- testeur_json.py
+++ testeur_json.py
@@ -15,7 +15,7 @@ class testeur_json(QMainWindow):
         
         self.lb_presentation = QLabel("Le ou les fichiers à tester doivent se trouver dans le répertoire 'test'.", self.zone_centrale) # Le label du haut
         self.lb_presentation.setGeometry(25, 20, 550, 40)
-        self.lb_presentation.setStyleSheet("font-size: 17px ; background: #E1F1D5 ; font-style: italic ; font-weight: bold")
+        self.lb_presentation.setStyleSheet("font-size: 17px ; font-style: italic ; font-weight: bold")
         self.lb_presentation.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
 
         self.liste_fichiers = [] # Création de la liste utilisé dans le ComboBox
@@ -27,35 +27,39 @@ class testeur_json(QMainWindow):
         self.cbb_fichier_teste.setGeometry(25, 80, 300, 60) # Coordonnées et taille
         self.cbb_fichier_teste.setEditable(True) # Permet d'exécuter setCurrentText, le setAlignement sur le ComboBox
         self.cbb_fichier_teste.lineEdit().setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
-        self.cbb_fichier_teste.setStyleSheet("font-size: 17px ; font-weight: bold")
+        self.cbb_fichier_teste.setStyleSheet("font-size: 17px ; font-weight: bold; border: 2px solid black")
         self.cbb_fichier_teste.addItems(self.liste_fichiers) # Ajoute dans le ComboBox tout se qui se trouve dans ma
         self.cbb_fichier_teste.setCurrentText("Choisir le fichier : ") # Ce qui apparait de base dans le comboBox
 
         self.btn_tester = QPushButton("TESTER", self.zone_centrale) # Le bouton
         self.btn_tester.setGeometry(387.5, 90, 150, 40)
-        self.btn_tester.setStyleSheet("background-color: blue ; border: 2px solid black ; border-radius: 75px 20px; font-size: 17px ; font-weight: bold")
+        self.btn_tester.setStyleSheet("""background-color: #FFE65A ; border: 1px solid black ; border-top-left-radius: 40px;
+                                    border-bottom-right-radius: 40px ; font-size: 17px ; font-weight: bold""")
         self.btn_tester.clicked.connect(self.test)
 
         self.lb_resultat = QLabel("Le résultat sera afficher ici.", self.zone_centrale) # Le label "RESULTAT"
         self.lb_resultat.setGeometry(25, 165, 550, 60)
-        self.lb_resultat.setStyleSheet("background: #E35CF1")
         self.lb_resultat.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
-        self.lb_resultat.setStyleSheet("font-size: 17px ; background: #E1F1D5 ; font-style: italic ; font-weight: bold")
+        self.lb_resultat.setStyleSheet("""font-size: 22px ; background: #E1F1D5 ; border-radius: 75px 20px ;
+                                    font-style: italic ; font-weight: bold ; border: 2px solid black""")
 
     def test(self): # Méthode qui test la validité du fichier.
         fichier = self.cbb_fichier_teste.currentText() # Récupère la valeur du comboBox donc le nom du fichier.
         if fichier == "Choisir le fichier : ": # Si aucun choix n'est fait dans le comboBox
-            self.lb_resultat.setStyleSheet("font-size: 17px ; color: black ; font-style: italic ; font-weight: bold")
+            self.lb_resultat.setStyleSheet("""font-size: 22px ; background: #E1F1D5 ; border-radius: 75px 20px ; 
+                                    font-style: italic ; font-weight: bold ; border: 2px solid black ; color: blue""")
             self.lb_resultat.setText("Vous n'avez pas sélectionné de fichier.")
         else:    
             try:
                 with open(f"test/{fichier}", "r") as f: # J'ouvre le fichier dans le TRY. Si ça fonctionne le fichier est valide.
                     essai = json.load(f)
                     f.close()
-                self.lb_resultat.setStyleSheet("font-size: 17px ; color: green ; font-style: italic ; font-weight: bold")
+                self.lb_resultat.setStyleSheet("""font-size: 22px ; background: #E1F1D5 ; border-radius: 75px 20px ;
+                                    font-style: italic ; font-weight: bold ; border: 2px solid black ; color: green""")
                 self.lb_resultat.setText("Le fichier est valide")
             except: # Sinon le fichier est invalide.
-                self.lb_resultat.setStyleSheet("font-size: 17px ; color: red ; font-style: italic ; font-weight: bold")
+                self.lb_resultat.setStyleSheet("""font-size: 22px ; background: #E1F1D5 ; border-radius: 75px 20px ; 
+                                    font-style: italic ; font-weight: bold ; border: 2px solid black ; color: red""")
                 self.lb_resultat.setText("Le fichier n'est pas valide")
 
 
